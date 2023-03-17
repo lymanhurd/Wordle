620d0fdf6a2473616156deb13d0c0b9a3d077f31
Glenn Hurd <glennqhurd@gmail.com>
2023-03-17 18:45:31 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,83 +1,97 @@
-import logging
+import json
+import math
 
-from dancing_links.links import solve
+# Opening JSON file
+f = open('wordle-answers.json')
+
+# returns JSON object as
+# a dictionary
+data = json.load(f)
+
+# Iterating through the json
+# list
+for i in data['words']:
+    print(i)
+
+# Closing file
+f.close()
 
 
-word_dict = {}
-
-
-def remove_duplicate_letters(lines):
+def score(guess, secret):
+    already_used = []
     result = []
-    for i in range(len(lines)):
-        lines[i] = lines[i].rstrip("\n")
-    for j in range(len(lines)):
-        if len(set(lines[j])) == 5:
-            result.append(lines[j])
+    for j in range(5):
+        for k in range(5):
+            if guess[j] not in already_used:
+                if guess[j] == secret[k] and j == k:
+                    result += 'g'
+                    already_used += guess[j]
+                elif guess[j] == secret[k]:
+                    result += 'y'
+                    already_used += guess[j]
+        if j >= len(result):
+            result += '-'
+    print(result)
+
+
+def tally_letters():
+    result = {"a": 0, "b": 0, "c": 0, "d": 0, "e": 0,
+              "f": 0, "g": 0, "h": 0, "i": 0, "j": 0,
+              "k": 0, "l": 0, "m": 0, "n": 0, "o": 0,
+              "p": 0, "q": 0, "r": 0, "s": 0, "t": 0,
+              "u": 0, "v": 0, "w": 0, "x": 0, "y": 0,
+              "z": 0}
+    for j in data["words"]:
+        for k in range(5):
+            result[j[k]] += 1
     return result
 
 
-def sort_and_remove_duplicates(lines):
-    result = []
-    new_lines = remove_duplicate_letters(lines)
-    for i in new_lines:
-        organized = ''.join(sorted(i))
-        if organized not in result:
-            result.append(organized)
-        if organized not in word_dict:
-            word_dict[organized] = [i]
+def total_words():
+    result = 0
+    for j in data["words"]:
+        result += 1
+    return result
+
+
+def find_probability():
+    result = tally_letters()
+    total = total_words()
+    for j in result.keys():
+        result[j] = result[j]/total
+    return result
+
+
+def find_information():
+    result = find_probability()
+    for j in result.keys():
+        if result[j] is 0:
+            result[j] = 0
         else:
-            word_dict[organized].append(i)
+            result[j] = -result[j] * math.log(result[j], 2)
     return result
 
 
-def break_word_into_vector(word):
-    result = [0] * 27
-    for i in word:
-        result[ord(i.lower()) - 97] = 1
+def sort_information():
+    x = find_information()
+    return {k: v for k, v in sorted(x.items(), key=lambda item: item[1], reverse=True)}
+
+
+def translate_guess(word):
+    info = sort_information()
+    result = 0
+    for j in word:
+        print(info[j])
+        result += info[j]
+        print(result)
     return result
 
 
-def convert_vector_into_word(vector):
-    result = ""
-    for i in range(26):
-        if vector[i] == 1:
-            result += chr(i + 97)
-    return result
-
-
-def alphabet_rows():
-    mrow = ([0] * 26) + [1]
-    matrix = []
-    for i in range(26):
-        matrix.append(mrow[:])
-        matrix[i][i] = 1
-    return matrix
-
-
-def get_row_name(word):
-    if len(word_dict[word]) == 1:
-        return word_dict[word][0]
-    else:
-        return ", ".join(word_dict[word])
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    with open('wordle.txt') as f:
-        lines = f.readlines()
-    # word_dict = {}
-    # print(len(lines))
-    deduped = remove_duplicate_letters(lines)
-    nodupelist = sort_and_remove_duplicates(lines)
-    # print(convert_vector_into_word(break_word_into_vector("ABCYZ")))
-    # print([w.strip() for w in lines if len(set(w)) == 5])
-    # print(word_dict)
-    row_names = [get_row_name(w) for w in word_dict.keys()]
-    word_matrix = [break_word_into_vector(w) for w in word_dict.keys()]
-    # print(word_matrix)
-    #test_list = break_word_into_vector("ilrsw")
-    #test_list = [1] + ([0] * 25) + [1]
-    #print_solution([test_list])
-    word_matrix += alphabet_rows()
-    row_names += [chr(i) for i in range(97, 123)]
-    solve(word_matrix, row_names=row_names)
+    score('abdce', 'abcde')
+    print(tally_letters())
+    print(total_words())
+    print(find_probability())
+    print(find_information())
+    print(sort_information())
+    print(translate_guess("radio"))
